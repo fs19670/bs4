90dde33418dbd2d6d7a7059f55233a58dadd0a08
fs19670 <92674223+fs19670@users.noreply.github.com>
2024-09-24 18:35:42 +0530

    Add files via upload

---
+++ portfolio.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+# Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+
+st.header("Hi, I am Faisal Shaikh:")
+st.title("A Python  Developer From Hyderabad")
+st.write(
+        "I am passionate about finding ways to use Python  to be more efficient and effective in business settings."
+    )
+st.write("[Learn More >](https://pythonandvba.com)")
+st.info("I am")
+st.write('I  ')
